5d752ceb41cbd73e695de52a1933b26a968aa481
CrossyChainsaw <kaangogcay@gmail.com>
2022-04-11 20:25:56 +0200

    Add Repetitions, this is the first working version

--- AutoClicker/Form1.Designer.cs
+++ AutoClicker/Form1.Designer.cs
@@ -31,19 +31,23 @@
             this.BTN_SetLocation = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.GB_FirstPoint = new System.Windows.Forms.GroupBox();
+            this.LBL_Ms = new System.Windows.Forms.Label();
+            this.LBL_delay = new System.Windows.Forms.Label();
+            this.NUD_Delay = new System.Windows.Forms.NumericUpDown();
             this.LBL_Y1 = new System.Windows.Forms.Label();
             this.LBL_X1 = new System.Windows.Forms.Label();
             this.BTN_Start = new System.Windows.Forms.Button();
             this.BTN_Add = new System.Windows.Forms.Button();
-            this.NUD_Delay = new System.Windows.Forms.NumericUpDown();
-            this.LBL_delay = new System.Windows.Forms.Label();
-            this.LBL_Ms = new System.Windows.Forms.Label();
+            this.NUD_Repetitions = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
             this.GB_FirstPoint.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.NUD_Delay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NUD_Repetitions)).BeginInit();
             this.SuspendLayout();
             // 
             // BTN_SetLocation
             // 
+            this.BTN_SetLocation.Cursor = System.Windows.Forms.Cursors.Hand;
             this.BTN_SetLocation.Location = new System.Drawing.Point(447, 9);
             this.BTN_SetLocation.Name = "BTN_SetLocation";
             this.BTN_SetLocation.Size = new System.Drawing.Size(114, 29);
@@ -76,49 +80,14 @@
             this.GB_FirstPoint.TabIndex = 2;
             this.GB_FirstPoint.TabStop = false;
             // 
-            // LBL_Y1
+            // LBL_Ms
             // 
-            this.LBL_Y1.AutoSize = true;
-            this.LBL_Y1.Location = new System.Drawing.Point(119, 13);
-            this.LBL_Y1.Name = "LBL_Y1";
-            this.LBL_Y1.Size = new System.Drawing.Size(47, 20);
-            this.LBL_Y1.TabIndex = 3;
-            this.LBL_Y1.Text = "y: null";
-            // 
-            // LBL_X1
-            // 
-            this.LBL_X1.AutoSize = true;
-            this.LBL_X1.Location = new System.Drawing.Point(66, 13);
-            this.LBL_X1.Name = "LBL_X1";
-            this.LBL_X1.Size = new System.Drawing.Size(47, 20);
-            this.LBL_X1.TabIndex = 2;
-            this.LBL_X1.Text = "x: null";
-            // 
-            // BTN_Start
-            // 
-            this.BTN_Start.Location = new System.Drawing.Point(694, 56);
-            this.BTN_Start.Name = "BTN_Start";
-            this.BTN_Start.Size = new System.Drawing.Size(94, 29);
-            this.BTN_Start.TabIndex = 3;
-            this.BTN_Start.Text = "Start";
-            this.BTN_Start.UseVisualStyleBackColor = true;
-            // 
-            // BTN_Add
-            // 
-            this.BTN_Add.Location = new System.Drawing.Point(694, 21);
-            this.BTN_Add.Name = "BTN_Add";
-            this.BTN_Add.Size = new System.Drawing.Size(94, 29);
-            this.BTN_Add.TabIndex = 4;
-            this.BTN_Add.Text = "Add";
-            this.BTN_Add.UseVisualStyleBackColor = true;
-            this.BTN_Add.Click += new System.EventHandler(this.BTN_Add_Click);
-            // 
-            // NUD_Delay
-            // 
-            this.NUD_Delay.Location = new System.Drawing.Point(284, 10);
-            this.NUD_Delay.Name = "NUD_Delay";
-            this.NUD_Delay.Size = new System.Drawing.Size(102, 27);
-            this.NUD_Delay.TabIndex = 5;
+            this.LBL_Ms.AutoSize = true;
+            this.LBL_Ms.Location = new System.Drawing.Point(387, 13);
+            this.LBL_Ms.Name = "LBL_Ms";
+            this.LBL_Ms.Size = new System.Drawing.Size(28, 20);
+            this.LBL_Ms.TabIndex = 7;
+            this.LBL_Ms.Text = "ms";
             // 
             // LBL_delay
             // 
@@ -129,20 +98,87 @@
             this.LBL_delay.TabIndex = 6;
             this.LBL_delay.Text = "delay:";
             // 
-            // LBL_Ms
+            // NUD_Delay
             // 
-            this.LBL_Ms.AutoSize = true;
-            this.LBL_Ms.Location = new System.Drawing.Point(387, 13);
-            this.LBL_Ms.Name = "LBL_Ms";
-            this.LBL_Ms.Size = new System.Drawing.Size(28, 20);
-            this.LBL_Ms.TabIndex = 7;
-            this.LBL_Ms.Text = "ms";
+            this.NUD_Delay.Location = new System.Drawing.Point(284, 10);
+            this.NUD_Delay.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.NUD_Delay.Name = "NUD_Delay";
+            this.NUD_Delay.Size = new System.Drawing.Size(102, 27);
+            this.NUD_Delay.TabIndex = 5;
+            this.NUD_Delay.ValueChanged += new System.EventHandler(this.NUD_Delay_ValueChanged);
+            // 
+            // LBL_Y1
+            // 
+            this.LBL_Y1.AutoSize = true;
+            this.LBL_Y1.Location = new System.Drawing.Point(119, 13);
+            this.LBL_Y1.Name = "LBL_Y1";
+            this.LBL_Y1.Size = new System.Drawing.Size(31, 20);
+            this.LBL_Y1.TabIndex = 3;
+            this.LBL_Y1.Text = "y: 0";
+            // 
+            // LBL_X1
+            // 
+            this.LBL_X1.AutoSize = true;
+            this.LBL_X1.Location = new System.Drawing.Point(66, 13);
+            this.LBL_X1.Name = "LBL_X1";
+            this.LBL_X1.Size = new System.Drawing.Size(31, 20);
+            this.LBL_X1.TabIndex = 2;
+            this.LBL_X1.Text = "x: 0";
+            // 
+            // BTN_Start
+            // 
+            this.BTN_Start.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BTN_Start.Location = new System.Drawing.Point(479, 21);
+            this.BTN_Start.Name = "BTN_Start";
+            this.BTN_Start.Size = new System.Drawing.Size(94, 29);
+            this.BTN_Start.TabIndex = 3;
+            this.BTN_Start.Text = "Start";
+            this.BTN_Start.UseVisualStyleBackColor = true;
+            this.BTN_Start.Click += new System.EventHandler(this.BTN_Start_Click);
+            // 
+            // BTN_Add
+            // 
+            this.BTN_Add.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BTN_Add.Location = new System.Drawing.Point(379, 21);
+            this.BTN_Add.Name = "BTN_Add";
+            this.BTN_Add.Size = new System.Drawing.Size(94, 29);
+            this.BTN_Add.TabIndex = 4;
+            this.BTN_Add.Text = "Add";
+            this.BTN_Add.UseVisualStyleBackColor = true;
+            this.BTN_Add.Click += new System.EventHandler(this.BTN_Add_Click);
+            // 
+            // NUD_Repetitions
+            // 
+            this.NUD_Repetitions.Location = new System.Drawing.Point(131, 21);
+            this.NUD_Repetitions.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.NUD_Repetitions.Name = "NUD_Repetitions";
+            this.NUD_Repetitions.Size = new System.Drawing.Size(102, 27);
+            this.NUD_Repetitions.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Repetitions:";
             // 
             // FRM_AutoClicker
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NUD_Repetitions);
             this.Controls.Add(this.BTN_Add);
             this.Controls.Add(this.BTN_Start);
             this.Controls.Add(this.GB_FirstPoint);
@@ -152,7 +188,9 @@
             this.GB_FirstPoint.ResumeLayout(false);
             this.GB_FirstPoint.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.NUD_Delay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NUD_Repetitions)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -168,5 +206,7 @@
         private Label LBL_Ms;
         private Label LBL_delay;
         private NumericUpDown NUD_Delay;
+        private NumericUpDown NUD_Repetitions;
+        private Label label2;
     }
 }
--- AutoClicker/Form1.cs
+++ AutoClicker/Form1.cs
@@ -1,56 +1,55 @@
+using System.Runtime.InteropServices;
+
 namespace AutoClicker
 {
     public partial class FRM_AutoClicker : Form
     {
-        // Properties
+        // No idea wtf this is but it works
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        public void DoMouseClick()
+        {
+            //Call the imported function with the cursor's current position
+            uint X = (uint)Cursor.Position.X;
+            uint Y = (uint)Cursor.Position.Y;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+        }
+
+
+        // Fields
         bool clicked = false;
         Point point1;
         int nGroupbox = 1;
         int verticalDistanceBetweenGroupboxes = 27;
         List<Point> pointList = new List<Point>();
+        List<int> delayList = new List<int>();
         Label lbl_x_temp;
         Label lbl_y_temp;
         int nTemp;
-        
+        int delayTemp;
 
-        // Constructor
+
+        // Methods
         public FRM_AutoClicker()
         {
             InitializeComponent();
             pointList.Add(new Point(0, 0));
+            delayList.Add(0);
         }
 
-        // Events
-
-        private void FRM_AutoClicker_Deactivate(object sender, EventArgs e)
-        {
-            if (clicked)
-            {
-                SavePoint(lbl_x_temp, lbl_y_temp, nTemp);
-                clicked = false;
-            }
-        }
-
-        private void BTN_SetLocation_Click(object sender, EventArgs e)
-        {
-            clicked = true;
-            lbl_x_temp = LBL_X1;
-            lbl_y_temp = LBL_Y1;
-        }
-
-        private void BTN_Add_Click(object sender, EventArgs e)
-        {
-            CreateGroupBox();
-        }
-
-        // Methods
         void CreateGroupBox()
         {
-            int y = GB_FirstPoint.Location.Y + verticalDistanceBetweenGroupboxes * nGroupbox;
-            int x = GB_FirstPoint.Location.X;
+            int yLocation = GB_FirstPoint.Location.Y + verticalDistanceBetweenGroupboxes * nGroupbox;
+            int xLocation = GB_FirstPoint.Location.X;
 
             GroupBox groupBox = new GroupBox();
-            groupBox.Location = new Point(12, y);
+            groupBox.Location = new Point(xLocation, yLocation);
             groupBox.Size = new Size(561, 38);
             groupBox.Text = "";
 
@@ -75,11 +74,11 @@ namespace AutoClicker
             lbl_point.Size = new Size(57, 20);
 
             lbl_x.Location = new Point(66, 13);
-            lbl_x.Text = "x: null";
+            lbl_x.Text = "x: 0";
             lbl_x.AutoSize = true;
 
             lbl_y.Location = new Point(119, 13);
-            lbl_y.Text = "y: null";
+            lbl_y.Text = "y: 0";
             lbl_y.AutoSize = true;
 
             lbl_delay.Location = new Point(236, 13);
@@ -88,6 +87,15 @@ namespace AutoClicker
 
             nud_delay.Location = new Point(284, 10);
             nud_delay.Size = new Size(102, 27);
+            nud_delay.Minimum = 0;
+            nud_delay.Maximum = 1000000000;
+            nud_delay.ValueChanged += (sender, e) =>
+            {
+                int nPoint = Convert.ToInt32(lbl_point.Text.Replace("Point ", "").Replace(":", ""));
+                int nDelay = nPoint - 1;
+                int delay = Convert.ToInt32(nud_delay.Value);
+                delayList[nDelay] = delay;
+            };
 
             lbl_ms.Location = new Point(387, 13);
             lbl_ms.Text = "ms";
@@ -96,12 +104,19 @@ namespace AutoClicker
             btn_SetLocation.Location = new Point(447, 9);
             btn_SetLocation.Text = "Set Location";
             btn_SetLocation.Size = new Size(114, 29);
+            btn_SetLocation.Cursor = Cursors.Hand;
             btn_SetLocation.Click += (sender, e) =>
             {
-                MessageBox.Show("the button was clicked");
+                clicked = true;
+                lbl_x_temp = lbl_x;
+                lbl_y_temp = lbl_y;
+                int nPoint = Convert.ToInt32(lbl_point.Text.Replace("Point ", "").Replace(":", ""));
+                nTemp = nPoint - 1;
             };
 
             Controls.Add(groupBox);
+            pointList.Add(new Point(0, 0));
+            delayList.Add(0);
 
             nGroupbox++;
         }
@@ -113,6 +128,48 @@ namespace AutoClicker
             lbl_x.Text = "x: " + cursorX;
             lbl_y.Text = "y: " + cursorY;
             pointList[nGroupbox] = new Point(cursorX, cursorY);
+        }
+
+        // Events
+        private void FRM_AutoClicker_Deactivate(object sender, EventArgs e)
+        {
+            if (clicked)
+            {
+                SavePoint(lbl_x_temp, lbl_y_temp, nTemp);
+                clicked = false;
+            }
+        }
+
+        private void BTN_SetLocation_Click(object sender, EventArgs e)
+        {
+            clicked = true;
+            lbl_x_temp = LBL_X1;
+            lbl_y_temp = LBL_Y1;
+        }
+
+        private void BTN_Add_Click(object sender, EventArgs e)
+        {
+            CreateGroupBox();
+        }
+
+        private void BTN_Start_Click(object sender, EventArgs e)
+        {
+            int repetitions = Convert.ToInt32(NUD_Repetitions.Value);
+            for (int i = 0; i < repetitions; i++)
+            {
+                for (int n = 0; n < delayList.Count; n++)
+                {
+                    Thread.Sleep(delayList[n]);
+                    Cursor.Position = pointList[n];
+                    DoMouseClick();
+                }
+            }
+        }
+
+        private void NUD_Delay_ValueChanged(object sender, EventArgs e)
+        {
+            int delay = Convert.ToInt32(NUD_Delay.Value);
+            delayList[0] = delay;
         }
     }
 }
